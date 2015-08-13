f113466259e1663219271fe5fa4a6e2d2dbfec1d
Brian Bidulock <bidulock@openss7.org>
2015-08-13 17:34:53 -0600

    applied archlinux hal.patch

--- hald/linux/probing/probe-video4linux.c
+++ hald/linux/probing/probe-video4linux.c
@@ -30,7 +30,7 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/ioctl.h>
-#include <linux/videodev.h>
+#include <libv4l1-videodev.h>
 #include <linux/videodev2.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -58,6 +58,8 @@ main (int argc, char *argv[])
 
 	setup_logger ();
 
+	dbus_error_init (&error);
+
 	device_file = getenv ("HAL_PROP_VIDEO4LINUX_DEVICE");
 	if (device_file == NULL)
 		goto out;
@@ -66,7 +68,6 @@ main (int argc, char *argv[])
 	if (udi == NULL)
 		goto out;
 
-	dbus_error_init (&error);
 	ctx = libhal_ctx_init_direct (&error);
 	if (ctx == NULL)
 		goto out;
